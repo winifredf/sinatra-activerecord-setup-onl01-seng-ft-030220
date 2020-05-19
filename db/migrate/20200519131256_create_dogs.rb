14c05154fb16334260bad7575003ddda19c294e0
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-19 13:16:12 +0000

    Automatically backed up by Learn

--- db/migrate/20200519131256_create_dogs.rb
+++ db/migrate/20200519131256_create_dogs.rb
@@ -1,9 +1,13 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :RUBY_RELEASE_DATE
+    end
   
   end
   
   def down
-  
+    drop_table :dogs
   end
 end
